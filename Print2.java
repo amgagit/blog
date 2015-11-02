e0bcdb5277764a58a7db14dfa787ec4b9df48bc7
Amgaa <monkhoo2011@gmail.com>
2015-11-02 12:03:08 +0800

    another java file is created

---
+++ Print2.java
@@ -0,0 +1,8 @@
+/* simple printing java app */
+
+public class Print2 {
+
+    public static void main(String[] args) {
+        System.out.println ("String is printed in Print2.java in blog T1");
+    }
+}
