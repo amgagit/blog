c3d28c9205aa0d83bbcd5a7e918a7e244bd7e961
Amgaa <monkhoo2011@gmail.com>
2015-11-02 15:08:11 +0800

    echo added

---
+++ Echo.java
@@ -0,0 +1,13 @@
+/* simple echo app */
+
+public class Echo {
+    public static void main(String[] args) {
+        String rlt = null;
+
+        for (int i = 1; i < args.length; i++) {
+            rlt += args[i] + " ";
+        }
+
+        System.out.println(rlt);
+    }
+}
